2d2c4981b6eec50e75a713abea810513c905b7f2
_pooh <natsathon2018@gmail.com>
2024-09-04 16:53:45 +0700

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,20 @@
 import { IconBrandFacebook, IconBrandFacebookFilled, IconBrandTwitter, IconBrandTwitterFilled } from "@tabler/icons-react";
 
 
+
 export default function ContactPage() {
   return (
     <div className="text-center my-4">
       <p className="fw-bold fs-4">Contact</p>
       <p>
         <strong>
-          <IconBrandFacebook /> Facebook :
+          <IconBrandFacebookFilled /> Facebook :
         </strong>
         TOP10IMDB
       </p>
       <p>
         <strong>
-          <IconBrandTwitter /> Twitter :
+          <IconBrandTwitterFilled /> Twitter :
         </strong>
         @TOP10IMDB
       </p>
