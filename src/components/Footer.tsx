913995e0ad364047e5b35c3f3137fed8e1c554d9
_pooh <natsathon2018@gmail.com>
2024-09-04 16:50:05 +0700

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer({year, fullName, studentId}: footerProps) {
   return (
     <div className="mt-3">
       <hr />
-      <p className="text-secondary text-center">Copyright © TOP-10-IMDB</p>
+      <p className="text-secondary text-center">Copyright © {year} {fullName} {studentId}</p>
     </div>
   );
 }
